c98e6e4aed5e6c783beaecc9a1ac7dcad8b8926d
YuTinH <57241071+YuTinH@users.noreply.github.com>
2025-04-06 23:08:23 +0800

    Update Eval_all.sh

--- scripts/CLMoE/Eval/Eval_all.sh
+++ scripts/CLMoE/Eval/Eval_all.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-bash ./scripts/VQACL/Eval/1_eval_recognition.sh
-bash ./scripts/VQACL/Eval/2_eval_location.sh
-bash ./scripts/VQACL/Eval/3_eval_judge.sh
-bash ./scripts/VQACL/Eval/4_eval_commonsense.sh
-bash ./scripts/VQACL/Eval/5_eval_count.sh
-bash ./scripts/VQACL/Eval/6_eval_action.sh
-bash ./scripts/VQACL/Eval/7_eval_color.sh
-bash ./scripts/VQACL/Eval/8_eval_type.sh
-bash ./scripts/VQACL/Eval/9_eval_subcategory.sh
-bash ./scripts/VQACL/Eval/10_eval_causal.sh
+bash ./scripts/CLMoE/Eval/1_eval_recognition.sh
+bash ./scripts/CLMoE/Eval/2_eval_location.sh
+bash ./scripts/CLMoE/Eval/3_eval_judge.sh
+bash ./scripts/CLMoE/Eval/4_eval_commonsense.sh
+bash ./scripts/CLMoE/Eval/5_eval_count.sh
+bash ./scripts/CLMoE/Eval/6_eval_action.sh
+bash ./scripts/CLMoE/Eval/7_eval_color.sh
+bash ./scripts/CLMoE/Eval/8_eval_type.sh
+bash ./scripts/CLMoE/Eval/9_eval_subcategory.sh
+bash ./scripts/CLMoE/Eval/10_eval_causal.sh
